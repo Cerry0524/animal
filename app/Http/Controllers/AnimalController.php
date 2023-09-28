ba417a180b6801495a6e1772522696fd97855ad6
Cerry0524 <128675853+Cerry0524@users.noreply.github.com>
2023-09-28 10:54:00 +0800

    建立排序功能

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -14,8 +14,32 @@ class AnimalController extends Controller
     public function index(Request $request)
     {
         $limit = $request->limit ?? 10;
-        $animals = Animal::orderBy('id', 'desc')
-        ->paginate($limit) //使用分頁功能，使用後資料會自動被DATA包起來
+
+        $query = Animal::query();//建立查詢建構器，分段撰寫SQL語句
+
+        //設定欄位查詢條件
+        if(isset($request->filters)){
+            $filters = explode(',',$request->filters);
+            foreach ($filters as $key => $filter) {
+                list($key,$value)=explode(':',$filter);
+                $query->where($key,'like',"%value%");
+            }
+        }
+        //設定排序條件
+        if(isset($request->sort)){
+            $sorts=explode(",",$request->sorts);
+            foreach ($sorts as $key => $sort) {
+                list($key,$value)=explode(":",$sort);
+                if($value=='asc' || $value=='desc'){
+                    $query->orderBy($key,$value);
+                }
+            }else{
+                $query->orderBy('id', 'desc')
+            }
+        }
+
+
+        $animals = $query->paginate($limit) //使用分頁功能，使用後資料會自動被DATA包起來
         ->appends($request->query());//回傳參數到URL＄後面方便使用
         return response($animals, Response::HTTP_OK);
     }
